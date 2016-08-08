3c152823aed32f71e3f74cc3edc8688a3414da9f
Ilan Frumer <ilanfrumer@gmail.com>
2016-08-08 13:27:23 +0300

    remove old code

--- src/MySQL.ts
+++ src/MySQL.ts
@@ -2,11 +2,7 @@ import { createConnection, IConnectionConfig } from 'mysql';
 import { MysqlConnection } from './MysqlConnection';
 
 export class MySQL {
-  private _ready: Promise<string>;
   private _connection = createConnection(this.config);
-
-  get ready() { return this._ready; }
-
   run(asyncFunction: (connection: MysqlConnection) => Promise<void>) {
 
     let disconnectCallback = () => this._connection.destroy();
